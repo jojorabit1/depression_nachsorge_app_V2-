571896e2c14c50bd8c675222f7d43d083580e87f
Joel Baudler <joelbaudler@pm.me>
2026-04-19 07:07:33 +0200

    L01: Onboarding-Eingaben mit Variablen und f-Strings

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def main():
+    name = input("Wie heißt du? ")
+    stimmung = int(input("Stimmung heute (1-10): "))
+    energie =int(input("Energie heute (1-10): "))
+    schlaf = int(input("Wie lange hast du geschlafen (in Stunden): "))
+
+    print(f"Hallo {name}. Stimmung: {stimmung}/10, Energie: {energie}/10, Schlaf: {schlaf}h.")
+
+if __name__ == "__main__":
+    main()
