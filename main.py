611fed41742a12c7a920c6f4f317fccd99abbfaf
Joel Baudler <joelbaudler@pm.me>
2026-05-04 16:07:43 +0200

    L04:Dictionaries - Wocheneingabe und Auswertung mit Stimmung/Energie/Schlaff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
+def stimmung():
     name = input("Wie heißt du? ")
     stimmung = int(input("Stimmung heute (1-10): "))
     energie =int(input("Energie heute (1-10): "))
@@ -13,7 +13,7 @@ def main():
         else:
             print(f"⚠️ Achtung {name}: Deine Werte sind heute kritisch niedrig. "
                   f"Bitte melde dich bei jemandem.")
-    elif stimmung < 4 or energie < 4 or schlaf < 5:
+    elif stimmung <= 4 or energie <= 4 or schlaf <= 5:
         print(f"Heute scheint es dir nicht ganz leicht zu fallen, {name}. Achte auf dich.")
     else:
         print(f"Gut gemacht, {name}. Deine Werte sind heute solide.")
@@ -59,7 +59,7 @@ def bewertung_erstellen(stimmung, energie):
     """Logik für das Frühwarnsystem (aus L02/L05)."""
     if stimmung < 4 and energie < 4:
         return "⚠️ Achtung: Deine Werte sind im kritischen Bereich."
-    elif stimmung < 4 or energie < 4:
+    elif stimmung < 6 or energie < 6:
         return "💡 Heute scheint ein schwieriger Tag zu sein. Achte auf dich."
     else:
         return "✅ Deine Werte sehen heute stabil aus."
@@ -95,10 +95,53 @@ def main():
 
 
 
+
+
+def eingabe_woche ():
+    woche = []
+    for tag_nummer in range(3):
+        print(f"Tag {tag_nummer + 1}")
+        tag = {
+            "stimmung": int(input(" Stimmung (1-10): ")),
+            "energie":  int(input(" Energie (1-10): ")),
+            "schlaf":   int(input(" Schlaf (1-10): "))
+        }
+        woche.append(tag)
+    return woche
+
+def ausgabe_woche(woche):
+    for i, tag in enumerate(woche):
+        print(f"Stimmung Tag {i+1}: {tag['stimmung']}")
+
+    stimmungswerte = []
+    for tag in woche:
+        stimmungswerte.append(tag["stimmung"])
+
+    durchschnitt = round(sum(stimmungswerte)/len(stimmungswerte),1)
+    print(f"Durchschnitt Stimmung: {durchschnitt}")
+
+    kritische_tage = 0
+    for tag in woche:
+        if tag["energie"] < 4:
+            kritische_tage = kritische_tage + 1
+
+    print(f"Tage mit niedriger Energie: {kritische_tage}")
+
+    if kritische_tage > 3:
+        print("⚠️ Diese Woche war belastend. Bitte sprich mit jemandem.")
+    elif kritische_tage >=1:
+        print("Diese Woche war gemischt. Achte auf dich")
+    else:
+        print("Gute Woche! Deine Energie war stabil")
+
+
 if __name__ == "__main__":
-     main()
+    # main()
     # stimmung_woche = [6, 4, 7, 3, 8, 2, 6]
     # auswertung_woche(stimmung_woche)
     # wochenwerte = eingabe_woche()
     # auswertung_woche(wochenwerte)
     # dictornary_test()
+    #stimmung()
+    wochenwerte = eingabe_woche()
+    ausgabe_woche(wochenwerte)
